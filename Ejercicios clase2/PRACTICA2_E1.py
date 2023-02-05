764fd4366bc85a4379ec05934ec0874b6385dc6d
HCSOSA <contrerassosahector@gmail.com>
2023-02-05 05:42:35 -0500

    Update PRACTICA2_E1.py

--- Ejercicios clase2/PRACTICA2_E1.py
+++ Ejercicios clase2/PRACTICA2_E1.py
@@ -8,21 +8,16 @@ mayores de edad.
 """
 print(msg)
 
-numeroOne=int(input("ingrese un valor: "))
+numeroDigitado=int(input("ingrese un valor: "))
 OpcionMenu=input('ingrese la opcion de menu: ')
 
 #menu
 
-print(numeroOne,OpcionMenu)
+print(numeroDigitado,OpcionMenu)
 
 if(OpcionMenu!='A' or OpcionMenu!='B' or OpcionMenu!='C'):
-    if OpcionMenu=='A':
-        for _ in range(4):
-            print('****')
-    elif OpcionMenu=='B':
-            #Realizar una iteración de una lista de números e identificar si es múltiplo de 2 e imprimir el número
-            numeroIterar=[2,5,6,8,10,2.3]
-    #elif OpcionMenu=='C':
-            #Iterar una lista de elementos que contengan nombre y edad e imprimir solo los mayores de edad
-else:
-    print('ingrese otro valor')
+    for _ in range(4):
+    print('****')
+elif numeroDigitado =='B':
+
+elif numeroDigitado =='C':
