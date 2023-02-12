f9583c75a598be153aea39bdf667cd99450f77e0
HCSOSA <contrerassosahector@gmail.com>
2023-02-11 23:51:43 -0500

    update

---
+++ EJERCICIOS CLASE3/LISTA_DE_EJERCICIO_NRO3/lib/E4.py
@@ -0,0 +1,4 @@
+""" 4. Una tienda de autopartes necesita un programa para catalogar sus productos ,crear la
+clase Catálogo y producto ,realizar un objeto dentro de un catálogo productos el cual debe
+tener un método para agregar productos y otra para mostrar toda la lista de productos.
+ """
--- EJERCICIOS CLASE3/LISTA_DE_EJERCICIO_NRO3/lib/main.py
+++ EJERCICIOS CLASE3/LISTA_DE_EJERCICIO_NRO3/lib/main.py
@@ -1,20 +1,38 @@
 __name__=='__main__'
 
-from E2 import *
-from E3 import *
-from E7 import *
-from E51 import *
-from E52 import *
+msg="""Elija una de las siguientes opciones:
+a)Ejecutar ejercicio2
+b)Ejecutar ejercicio3
+c)Ejecutar ejercicio4
+d)Ejecutar ejercicio7
+e)Ejecutar ejercicio5.1
+f)Ejecutar ejercicio5.2"""
+print(msg)
 
-from prueba import *
-from E4_ProOriObj import *
+OpcionMenu=input("ingrese la opcion de menu: ")
 
-saludar()
+if(OpcionMenu!='a' or OpcionMenu!='b' or OpcionMenu!='c' or OpcionMenu!='d' or OpcionMenu!='e'or OpcionMenu!='f'):
+    if OpcionMenu=='a':
+        from E2 import *
+        print('ejecutando ejercicio2')
 
-
-c3=Celular('apple','retina488','chi2323','1,5,8')
-print(c3)
-
-print(p1)
-print(p2)
-
+    elif OpcionMenu!='b':
+        from E3 import *
+        print('ejecutando ejercicio3')
+    
+    elif OpcionMenu!='c':
+        print('ejecutando ejercicio4')
+    
+    elif OpcionMenu!='d':
+        from E7 import *
+        print('ejecutando ejercicio7')
+    
+    elif OpcionMenu!='e':
+        from E51 import *
+        print('ejecutando ejercicio 5.1')
+    
+    elif OpcionMenu!='f':
+        from E52 import *
+        print('ejecutando ejercicio 5.2')
+else:
+    print("digite caracter correcto")
